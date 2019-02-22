52636add73a94c33b2ce4e344642bdd4513ef4d6
yash.rana <y.rana18@outlook.com>
2019-02-22 17:43:26 +0530

    Add project files.

---
+++ ImageProcessing/frmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ImageProcessing
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.frmImage = new System.Windows.Forms.PictureBox();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplasianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplasianToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageSmoothingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inverseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.frmImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.filtersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(603, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.frmImage);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(579, 509);
+            this.panel1.TabIndex = 2;
+            // 
+            // frmImage
+            // 
+            this.frmImage.Location = new System.Drawing.Point(3, 3);
+            this.frmImage.Name = "frmImage";
+            this.frmImage.Size = new System.Drawing.Size(535, 470);
+            this.frmImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.frmImage.TabIndex = 1;
+            this.frmImage.TabStop = false;
+            this.frmImage.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.frmImage_LoadCompleted);
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.laplasianToolStripMenuItem,
+            this.laplasianToolStripMenuItem1,
+            this.imageSmoothingToolStripMenuItem,
+            this.inverseToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // laplasianToolStripMenuItem
+            // 
+            this.laplasianToolStripMenuItem.Name = "laplasianToolStripMenuItem";
+            this.laplasianToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.laplasianToolStripMenuItem.Text = "GrayScale";
+            this.laplasianToolStripMenuItem.Click += new System.EventHandler(this.laplasianToolStripMenuItem_Click);
+            // 
+            // laplasianToolStripMenuItem1
+            // 
+            this.laplasianToolStripMenuItem1.Name = "laplasianToolStripMenuItem1";
+            this.laplasianToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.laplasianToolStripMenuItem1.Text = "Laplasian";
+            this.laplasianToolStripMenuItem1.Click += new System.EventHandler(this.laplasianToolStripMenuItem1_Click);
+            // 
+            // imageSmoothingToolStripMenuItem
+            // 
+            this.imageSmoothingToolStripMenuItem.Name = "imageSmoothingToolStripMenuItem";
+            this.imageSmoothingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.imageSmoothingToolStripMenuItem.Text = "Image Smoothing";
+            this.imageSmoothingToolStripMenuItem.Click += new System.EventHandler(this.imageSmoothingToolStripMenuItem_Click);
+            // 
+            // inverseToolStripMenuItem
+            // 
+            this.inverseToolStripMenuItem.Name = "inverseToolStripMenuItem";
+            this.inverseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inverseToolStripMenuItem.Text = "Inverse";
+            this.inverseToolStripMenuItem.Click += new System.EventHandler(this.inverseToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 548);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Image Processing App";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.frmImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox frmImage;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplasianToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplasianToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem imageSmoothingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inverseToolStripMenuItem;
+    }
+}
+
---
+++ ImageProcessing/frmMain.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+     
+    public struct PixelData
+    {
+        public Byte Blue, Green, Red;
+    };
+
+    unsafe public partial class frmMain : Form
+    {
+        
+            PixelData*[,] ImagaData;
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Bitmap File|*.bmp|Jpeg Images|*.jpeg;*.jpg|Png Images|*.png";
+
+            if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                frmImage.ImageLocation = dialog.FileName;
+            }
+
+        }
+
+        private void laplasianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            PixelData* dstPointer;
+            for(int i=0;i<frmImage.Height;i++)
+            {
+                for(int j=0;j<frmImage.Width;j++)
+                {
+                    dstPointer = ImagaData[i, j];
+                    dstPointer->Green = dstPointer->Red;
+                    dstPointer->Blue = dstPointer->Red;
+
+                }
+            }
+
+            frmImage.Refresh();
+        }
+
+        private void frmImage_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            var bitmap = (Bitmap)frmImage.Image;
+            var clone = new Bitmap(bitmap.Width, bitmap.Height, PixelFormat.Format24bppRgb);
+            Graphics gr = Graphics.FromImage(clone);
+            gr.DrawImage(bitmap, new Rectangle(0, 0, clone.Width, clone.Height));
+            frmImage.Image = clone;
+
+            var obj = (Bitmap)frmImage.Image;
+            BitmapData data;
+            data = obj.LockBits(new Rectangle(0, 0, obj.Width, obj.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            ImagaData = new PixelData*[data.Height, data.Width];
+
+            PixelData* dstPointer = (PixelData*)data.Scan0;
+            for (int i = 0; i < obj.Height; i++)
+            {
+                for (int j = 0; j < obj.Width; j++)
+                {
+
+                    ImagaData[i, j] = dstPointer;
+                    dstPointer = dstPointer + 1;
+                }
+            }
+
+            obj.UnlockBits(data);
+
+        }
+
+        private void laplasianToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            byte[,] pixelValue=new byte[frmImage.Height,frmImage.Width];
+            int temp;
+            for(int i = 1; i < frmImage.Height - 2; i++)
+            {
+                for(int j = 1; j < frmImage.Width - 2;j++)
+                {
+                    temp = ( Math.Abs((ImagaData[i, j]->Red * 4)+(-1*ImagaData[i - 1, j]->Red + -1 * ImagaData[i + 1, j]->Red + -1 * ImagaData[i, j - 1]->Red + -1 * ImagaData[i, j + 1]->Red)));
+                    if(temp>255)
+                    {
+                        temp = 255;
+                    }
+                    pixelValue[i, j] = Convert.ToByte(temp);
+                }
+            }
+            for (int i = 1; i < frmImage.Height - 2; i++)
+            {
+                for (int j = 1; j < frmImage.Width - 2; j++)
+                {
+                    ImagaData[i, j]->Red = pixelValue[i, j];
+                    ImagaData[i, j]->Green = pixelValue[i, j];
+                    ImagaData[i, j]->Blue = pixelValue[i, j];
+
+                }
+            }
+            frmImage.Refresh();
+        }
+
+        private void imageSmoothingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[,] rpixelValue = new byte[frmImage.Height, frmImage.Width];
+            byte[,] gpixelValue = new byte[frmImage.Height, frmImage.Width];
+            byte[,] bpixelValue = new byte[frmImage.Height, frmImage.Width];
+            double temp;
+            for (int i = 1; i < frmImage.Height - 2; i++)
+            {
+                for (int j = 1; j < frmImage.Width - 2; j++)
+                {
+                    temp = (ImagaData[i,j]->Red + ImagaData[i+1, j]->Red + ImagaData[i-1, j]->Red+ ImagaData[i, j+1]->Red + ImagaData[i, j-1]->Red + ImagaData[i+1, j+1]->Red + ImagaData[i+1, j-1]->Red+ ImagaData[i-1, j+1]->Red + ImagaData[i-1, j-1]->Red) / 9;
+                    rpixelValue[i, j] = Convert.ToByte(temp);
+
+
+                    temp = (ImagaData[i, j]->Green + ImagaData[i + 1, j]->Green + ImagaData[i - 1, j]->Green + ImagaData[i, j + 1]->Green + ImagaData[i, j - 1]->Green + ImagaData[i + 1, j + 1]->Green + ImagaData[i + 1, j - 1]->Green + ImagaData[i - 1, j + 1]->Green + ImagaData[i - 1, j - 1]->Green) / 9;
+                    gpixelValue[i, j] = Convert.ToByte(temp);
+
+                    temp = (ImagaData[i, j]->Blue + ImagaData[i + 1, j]->Blue + ImagaData[i - 1, j]->Blue + ImagaData[i, j + 1]->Blue + ImagaData[i, j - 1]->Blue + ImagaData[i + 1, j + 1]->Blue + ImagaData[i + 1, j - 1]->Blue + ImagaData[i - 1, j + 1]->Blue + ImagaData[i - 1, j - 1]->Blue) / 9;
+                    bpixelValue[i, j] = Convert.ToByte(temp);
+
+
+                }
+            }
+            for (int i = 1; i < frmImage.Height - 2; i++)
+            {
+                for (int j = 1; j < frmImage.Width - 2; j++)
+                {
+                    ImagaData[i, j]->Red = rpixelValue[i, j];
+                    ImagaData[i, j]->Green = gpixelValue[i, j];
+                    ImagaData[i, j]->Blue = bpixelValue[i, j];
+
+                }
+            }
+            frmImage.Refresh();
+
+        }
+
+        private void inverseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < frmImage.Height; i++)
+            {
+                for (int j = 0; j < frmImage.Width; j++)
+                {
+                    ImagaData[i, j]->Red = Convert.ToByte(255 - ImagaData[i,j]->Red);
+                    ImagaData[i, j]->Green = Convert.ToByte(255 - ImagaData[i, j]->Green);
+                    ImagaData[i, j]->Blue =  Convert.ToByte(255 - ImagaData[i, j]->Blue);
+
+                }
+            }
+
+            frmImage.Refresh();
+        }
+    }
+}
